41d4b637860083c01dc6ca9c8557afcaaab02af8
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-12-02 22:10:37 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(function(){
   $('#console-box').append('console is ready...');
   
-  $('btn btn-light nonnum').click(function(){
+  $('button').click(function(){
     $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
     $(this).toggleClass('selected').siblings().removeClass('selected');
 ; 
