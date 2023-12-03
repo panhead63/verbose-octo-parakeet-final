280c0e1858dc548f3a9443a44138828de800b386
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-12-02 22:17:57 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,36 @@
 $(function(){
-  $('#console-box').append('console is ready...');
+  $('#console-box').append('<br/>console is ready...');
   
   $('button').click(function(){
-    $('#console-box').append('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
-    $(this).toggleClass('selected').siblings().removeClass('selected');
+    $('#console-box').prepend('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
 ; 
   });
   
+});
+
+
+$('.grade').click(function(){
+  
+    $('.numdetail').removeClass('notAvailable initialAvailable');
+    
+    $('#plus').empty().append($(this).attr("id") + '7-' + $(this).attr("id")+'9+');
+  
+    $('#neutral').empty().append($(this).attr("id") + '3-' + $(this).attr("id")+'6');
+  
+    $('.detail').removeClass('notAvailable initialAvailable');
+  
+    $('#minus').empty().append($(this).attr("id") + '0-' + $(this).attr("id")+'2');
+; 
+});
+
+
+$('.nonnum').click(function(){
+  
+    $('.numdetail').addClass('notAvailable');
+    $('.detail').removeClass('initialAvailable');
+  
+    $('#plus').empty().append('-');
+    $('#minus').empty().append('-');
+    $('#neutral').empty().append('-');
+;
 });
